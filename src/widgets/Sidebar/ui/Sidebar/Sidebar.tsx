6b1b554ecdfa304726a36e2f04a7bd92dcbf29c8
mikadze giorgi <mikadzegio9000@gmail.com>
2024-08-06 23:59:17 +0400

    fix linter

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,10 +4,13 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
+import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
     className?: string;
 }
+
+const { t } = useTranslation();
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
@@ -25,7 +28,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t('переключать')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
